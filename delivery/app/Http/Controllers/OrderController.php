b5ee5d9eaa31878baeb882899c9f27a82e702fe6
goddanshi <oceanfluddy@gmail.com>
2025-03-22 15:04:50 +0300

    Изменение POST запроса чтоб выводил объект массиом, а не строкой. Подсчет тотал прайса

--- delivery/app/Http/Controllers/OrderController.php
+++ delivery/app/Http/Controllers/OrderController.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\Models\Order;
+use App\Models\Pizza;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
@@ -11,9 +11,30 @@ class OrderController extends Controller
     public function index()
     {
         $orders = Order::all();
-        return response()->json($orders);
+    
+        return response()->json($orders->map(function ($order) {
+            $orderData = $order->toArray();
+            $korzina = json_decode($order->korzina, true);
+    
+            if (!is_array($korzina)) {
+                $korzina = [];
+            }
+    
+            $orderData['korzina'] = array_map(function ($item) {
+                $pizza = Pizza::find($item['pizza_id']);
+                return [
+                    'pizza_id' => $item['pizza_id'],
+                    'name' => $pizza ? $pizza->name : 'Неизвестная пицца',
+                    'opisanie' => $pizza ? $pizza->opisanie : 'Описание отсутствует',
+                    'quantity' => $item['quantity'],
+                    'price' => $pizza ? $pizza->price : 0,
+                    'total_price' => $pizza ? $pizza->price * $item['quantity'] : 0
+                ];
+            }, $korzina);
+    
+            return $orderData;
+        }));
     }
-
     // Создание нового заказа
     public function store(Request $request)
     {
@@ -26,8 +47,6 @@ class OrderController extends Controller
             'korzina.*.pizza_id' => 'required|integer',  // Пицца должна быть в корзине
             'korzina.*.quantity' => 'required|integer|min:1',
         ]);
-
-        // Создание нового заказа
         $order = Order::create([
             'nameOrder' => $validated['nameOrder'],
             'phone' => $validated['phone'],
