27b6b6296c70ab8aba5ce71cf8d5a65c0499e607
goddanshi <oceanfluddy@gmail.com>
2025-03-24 22:06:48 +0300

    Переделка body сайта, создание объекта pizzas и подставление его свойств recode

--- delivery/frontend/src/components/PizzaRow1.vue
+++ delivery/frontend/src/components/PizzaRow1.vue
@@ -1,79 +1,41 @@
 <template>
-<div class="body1">     
-        <div class="nadpis"><p>Пицца на любой ваш вкус</p></div>
-    <div class="pizzas-container1">
-        <div class="image-container">
-            <img :src="vetchinaikurica" alt="Pizza" />
-            <p class="image-caption">Ветчина и курица</p> 
-            <p class="image-description">Тесто для пиццы, соус сливочный, сыр моцарелла, куриное филе, ветчина, помидоры.</p>
-            <p class="image-price">660 р.</p>
-            <button class="image-button">Заказать</button>
+    <div class="body1">
+      <div class="nadpis"><p>Пицца на любой ваш вкус</p></div>
+      <div class="pizzas-container1">
+        <div class="image-container" v-for="(pizza, index) in pizzas" :key="index">
+          <img :src="pizza.image" :alt="pizza.name">
+          <p class="image-caption">{{ pizza.name }}</p>
+          <p class="image-description">{{ pizza.description }}</p>
+          <p class="image-price">{{ pizza.price }} р.</p>
+          <button class="image-button">Заказать</button>
         </div>
-        
-        <div class="image-container">
-            <img :src="peperoni" alt="Пепперони">
-            <p class="image-caption">Пепперони</p> 
-            <p class="image-description">Тесто для пиццы, соус фирменный, сыр моцарелла, салями оригинальная.</p>
-            <p class="image-price">600 р.</p>
-            <button class="image-button">Заказать</button>
-        </div>
-
-        <div class="image-container">
-            <img :src="margharita" alt="Маргарита">
-             <p class="image-caption">Маргарита</p> 
-            <p class="image-description">Тесто для пиццы, соус фирменный, сыр моцарелла, помидоры.</p><br> <!-- br Для правильного отступа -->
-            <p class="image-price">490 р.<s id="opac">560 р.</s></p>
-            <button class="image-button">Заказать</button>
-        </div>
+      </div>
     </div>
-    <div class="pizzas-container2">
-        <div class="image-container">
-            <img :src="asorti" alt="Pizza" />
-            <p class="image-caption">Асорти</p> 
-            <p class="image-description">Тесто для пиццы, ветчина, бекон, маслины, помидоры, перец свежий, сыр моцарелла, соус фирменный, филе куриное.</p>
-            <p class="image-price">770 р.</p>
-            <button class="image-button">Заказать</button>
-        </div>
-        
-        <div class="image-container">
-            <img :src="greze" alt="Пепперони">
-            <p class="image-caption">Греческая</p> 
-            <p class="image-description">Тесто для пиццы, соус фирменный, лук, маслины, помидоры, брынза, пекинская капуста, зелень, перец болгарский, сыр моцарелла.</p>
-            <p class="image-price">630 р.</p>
-            <button class="image-button">Заказать</button>
-        </div>
-
-        <div class="image-container">
-            <img :src="teriyaki" alt="Маргарита">
-             <p class="image-caption">Цыпленок Терияки</p> 
-            <p class="image-description">Тесто для пиццы, соус ранч, помидоры, сыр моцарелла, куриное филе, огурцы свежие, соус терияки.</p><br> <!-- br Для правильного отступа -->
-            <p class="image-price">680 р.</p>
-            <button class="image-button">Заказать</button>
-        </div>
-    </div>
-</div>
-</template>
-<script>
-    import vetchinaikurica from '@/assets/Vetchinaikurica-280x280.jpg';
-    import peperoni from '@/assets/Peperoni.jpg';
-    import margharita from '@/assets/Margharita.jpg';
-    import asorti from '@/assets/Asorti.jpg';
-    import greze from '@/assets/Greze.jpg';
-    import teriyaki from '@/assets/Tireyaki.jpg';
-    export default { 
-        name: "PizzaRow1",
-        data(){
-        return {//Создание свойства чтоб работала локальная фотка (Иначе выводил alt..)
-        margharita,
-        peperoni,
-        vetchinaikurica,
-        asorti,
-        greze,
-        teriyaki,
-    };
-  }
-}
-</script>
+  </template>
+  
+  <script>
+  import vetchinaikurica from "../assets/Vetchinaikurica-280x280.jpg"
+  import peperoni from "../assets/Peperoni.jpg"
+  import margharita from "../assets/Margharita.jpg"
+  import asorti from "../assets/Asorti.jpg"
+  import greze from "../assets/Greze.jpg"
+  import tireyaki from "../assets/Tireyaki.jpg"
+  export default {
+    props: ['addToCart'],
+    data() {
+      return {
+        pizzas: [
+          { name: "Ветчина и курица", image: vetchinaikurica, description: "Тесто, соус сливочный, сыр моцарелла, куриное филе, ветчина, помидоры.", price: 660 },
+          { name: "Пепперони", image: peperoni, description: "Тесто, соус фирменный, сыр моцарелла, салями оригинальная.", price: 600 },
+          { name: "Маргарита", image: margharita, description: "Тесто, соус фирменный, сыр моцарелла, помидоры.", price: 490 },
+          { name: "Ассорти", image: asorti, description: "Тесто, ветчина, бекон, маслины, помидоры, перец, сыр моцарелла, соус фирменный, филе куриное.", price: 770 },
+          { name: "Греческая", image: greze, description: "Тесто, соус фирменный, лук, маслины, помидоры, брынза, пекинская капуста, зелень, перец, сыр моцарелла.", price: 630 },
+          { name: "Цыпленок Терияки", image: tireyaki, description: "Тесто, соус ранч, помидоры, сыр моцарелла, куриное филе, огурцы, соус терияки.", price: 680 }
+        ]
+      };
+    }
+  };
+  </script>
 <style>
 .body1{
     padding-top: 100px;
@@ -84,19 +46,12 @@
     text-align: center;
     font-size: 50px;
 }
-.pizzas-container1 {
-  display: flex;  
-  justify-content: center; 
-  gap: 40px; 
+.pizzas-container1, .pizzas-container2 {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr); /* 3 колонки */
+  justify-content: center;
+  gap: 40px;
   padding: 20px;
-  flex-wrap: wrap; 
-}
-.pizzas-container2 {
-  display: flex;  
-  justify-content: center; 
-  gap: 40px; 
-  padding: 20px;
-  flex-wrap: wrap; 
 }
 .image-container {
   display: flex;
